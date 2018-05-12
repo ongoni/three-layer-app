ff517787ccc1c8d6f65ebf9884fb14017feae038
ongoni <ongoni.fg@gmail.com>
2018-05-12 11:12:56 +0400

    add consoleUI project

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
