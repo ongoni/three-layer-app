96689c3d928bb107ba2517029d41f634b87f4394
ongoni <ongoni.fg@gmail.com>
2018-05-19 01:02:26 +0400

    add methods to contracts

--- DataAccessContracts/IDataAccess.cs
+++ DataAccessContracts/IDataAccess.cs
@@ -1,7 +1,28 @@
-﻿namespace DataAccessContracts
+﻿using System.Collections.Generic;
+using Entities;
+
+namespace DataAccessContracts
 {
     public interface IDataAccess
     {
-        
+        bool Add(Person person);
+        bool Add(Medal medal);
+        bool Add(Material material);
+        bool Add(Address address);
+        void AddMedalToPerson(Person person, Medal medal);
+        IEnumerable<Person> GetAllPersons();
+        IEnumerable<Medal> GetAllMedals();
+        IEnumerable<Address> GetAllAddresses();
+        IEnumerable<Material> GetAllMaterials();
+        IEnumerable<Medal> GetPersonMedalsByPersonId(int id);
+        IEnumerable<Address> GetPersonAddressesByPersonId(int id);
+        Person GetPersonById(int id);
+        Medal GetMedalById(int id);
+        Address GetAddressById(int id);
+        Material GetMaterialById(int id);
+        bool DeletePersonById(int id);
+        bool DeleteMedalById(int id);
+        bool DeleteAddressById(int id);
+        bool DeleteMaterialById(int id);
     }
 }
--- LogicContracts/ILogic.cs
+++ LogicContracts/ILogic.cs
@@ -1,7 +1,29 @@
-﻿namespace LogicContracts
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+
+namespace LogicContracts
 {
     public interface ILogic
     {
-        
+        List<Person> GetAllPersons();
+        List<Medal> GetAllMedals();
+        List<Address> GetAllAddresses();
+        List<Material> GetAllMaterials();
+        Person GetPersonById(int id);
+        Medal GetMedalById(int id);
+        Address GetAddressById(int id);
+        Material GetMaterialById(int id);
+        List<Medal> GetPersonMedals(int id);
+        Person SavePerson(string firstName, string lastName, DateTime birthDate, int age);
+        Person SavePerson(string firstName, string lastName, DateTime birthDate, int age, List<Address> addresses);
+        Medal SaveMedal(string name, Material material);
+        Address SaveAddress(string city, string street, string houseNumber);
+        Material SaveMaterial(string name);
+        void AddMedalToPerson(Person person, Medal medal);
+        bool DeletePerson(int id);
+        bool DeleteMedal(int id);
+        bool DeleteAddress(int id);
+        bool DeleteMaterial(int id);
     }
 }
