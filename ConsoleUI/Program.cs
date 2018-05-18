add6d132ac193a1b597117b96aa25cf0c97cad4c
ongoni <ongoni.fg@gmail.com>
2018-05-19 01:03:57 +0400

    add console ui methods

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using AppConfig;
+using Entities;
 using LogicContracts;
 using Ninject;
 
@@ -73,37 +74,125 @@ namespace ConsoleUI
 
         private static void AddPerson()
         {
+            Console.Write("Enter first name: ");
+            string firstName = Console.ReadLine();
             
+            Console.Write("Enter last name: ");
+            string lastName = Console.ReadLine();
+            
+            Console.Write("Enter birth date: ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            
+            Console.Write("Enter age: ");
+            if (int.TryParse(Console.ReadLine(), out var age))
+            {
+                throw new ArgumentException("incorrect age");
+            }
+
+            Person person = logic.SavePerson(firstName, lastName, birthDate, age);
         }
 
         private static void AddMedal()
         {
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
             
-        }
+            Console.WriteLine("Choose material:");
+            ShowMaterials();
+            
+            if (int.TryParse(Console.ReadLine(), out var id) || logic.GetMaterialById(id) == null)
+            {
+                throw new ArgumentException("incorrect id");
+            }
 
-        private static void DeletePerson()
-        {
-            
-        }
-        
-        private static void DeleteMedal()
-        {
-            
+            Medal medal = logic.SaveMedal(name, logic.GetMaterialById(id));
         }
 
         private static void AddMedalToPerson()
         {
+            Console.WriteLine("Choose medal:");
+            ShowMedals();
             
+            if (int.TryParse(Console.ReadLine(), out var medalId) || logic.GetMedalById(medalId) == null)
+            {
+                throw new ArgumentException("incorrect id");
+            }
+            
+            Console.WriteLine("Choose person:");
+            ShowPersons();
+            
+            if (int.TryParse(Console.ReadLine(), out var personId) || logic.GetPersonById(personId) == null)
+            {
+                throw new ArgumentException("incorrect id");
+            }
+            
+            logic.AddMedalToPerson(logic.GetPersonById(personId), logic.GetMedalById(medalId));
         }
 
+        private static void ShowPersons()
+        {
+            foreach (Person person in logic.GetAllPersons())
+            {
+                Console.WriteLine(person);
+            }
+        }
+        
         private static void ShowMedals()
         {
-            
+            foreach (Medal medal in logic.GetAllMedals())
+            {
+                Console.WriteLine(medal);
+            }
+        }
+        
+        private static void ShowMaterials()
+        {
+            foreach (Material material in logic.GetAllMaterials())
+            {
+                Console.WriteLine(material);
+            }
         }
 
         private static void ShowMedalsForPerson()
         {
+            Console.WriteLine("Choose person:");
+            ShowPersons();
             
+            if (int.TryParse(Console.ReadLine(), out var id) || logic.GetPersonById(id) == null)
+            {
+                throw new ArgumentException("incorrect id");
+            }
+            
+            foreach (Medal medal in logic.GetPersonMedals(id))
+            {
+                Console.WriteLine(medal);
+            }
+        }
+        
+        private static void DeletePerson()
+        {
+            Console.WriteLine("Choose person:");
+            ShowPersons();
+            
+            if (int.TryParse(Console.ReadLine(), out var id) || logic.GetPersonById(id) == null)
+            {
+                throw new ArgumentException("incorrect id");
+            }
+
+            bool result = logic.DeletePerson(id);
+        }
+        
+        private static void DeleteMedal()
+        {
+            Console.WriteLine("Choose medal:");
+            ShowMedals();
+            
+            if (int.TryParse(Console.ReadLine(), out var id) || logic.GetMedalById(id) == null)
+            {
+                throw new ArgumentException("incorrect id");
+            }
+
+            bool result = logic.DeleteMedal(id);
         }
     }
 }
